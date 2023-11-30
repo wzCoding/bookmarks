92aadf5d9f05c7a7ec58f25c5001d0aeb4d9e2e8
wzCoding <1429354968@qq.com>
2023-11-30 19:19:39 +0800

    submit the code

---
+++ bookmark/src/components/bookmark.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="bookmark-item" @click="handleClick">
+        <slot>
+            <img class="bookmark-icon" :src="iconUrl" alt="bookmark-icon">
+            <div class="bookmark-title">{{ title }}</div>
+        </slot>
+    </div>
+</template>
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+const props = defineProps({
+    title: {
+        type: String,
+        default: ""
+    },
+    iconUrl: {
+        type: String,
+        default: "./icons/folder.png"
+    }
+});
+const emit = defineEmits(['open']);
+const handleClick = (e) => {
+    emit("open", e);
+}
+</script>
+<style lang="scss">
+.bookmark-item {
+    cursor: pointer;
+    font-size: .75rem;
+    padding: .5rem;
+    position: relative;
+    z-index: 1;
+    border-radius: 5px;
+    transition: all 0.3s cubic-bezier(0.33, -0.16, 0.58, 1);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    &:hover {
+        box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+        transform: scale(1.2);
+        background: rgba(255, 255, 255, 0.3);
+        // font-weight: 600;
+        // font-size: .8rem;
+    }
+
+    .bookmark-icon {
+        width: 2.5rem;
+    }
+
+    .bookmark-title {
+        padding-top: .25rem;
+    }
+}
+</style>
--- bookmark/src/views/HomeView.vue
+++ bookmark/src/views/HomeView.vue
@@ -3,10 +3,8 @@
         <div class="bookmark-container">
             <div class="bookmark-header"></div>
             <div class="bookmark-content">
-                <div class="bookmark-item" v-for="bookmark in bookmarks" :key="bookmark.id">
-                    <img class="bookmark-icon" :src="bookmark.url" alt="bookmark-icon">
-                    <div class="bookmark-title">{{ bookmark.title }}</div>
-                </div>
+                <bookmark @open="openBookMark" v-for="bookmark in bookmarks" :key="bookmark.id" :icon-url="bookmark.url" :title="bookmark.title">
+                </bookmark>
             </div>
         </div>
     </div>
@@ -14,24 +12,18 @@
 
 <script setup>
 import { ref } from 'vue';
+import bookmark from '@/components/bookmark.vue';
 const bookmarks = ref([]);
-const handleTree = (tree) => {
-    for (const treeNode of tree) {
-        if (treeNode.children && !treeNode.url) {
-            treeNode.url = "./icons/folder.png";
-            handleTree(treeNode.children);
-        }
-    }
-    return tree;
+const openBookMark = (e)=>{
+    console.log(e.target)
 }
 const getData = async (result) => {
     if (result) {
-        bookmarks.value = handleTree(result[0].children);
+        bookmarks.value = result[0].children;
     } else {
         const data = await import('../../public/utils/data.json');
-        bookmarks.value = handleTree(data.default[0].children);
+        bookmarks.value = data.default[0].children;
     }
-    console.log(bookmarks.value)
 }
 getData();
 //chrome.bookmarks.getTree(getData)
@@ -51,61 +43,33 @@ getData();
         border-radius: 5px;
         background-color: transparent;
         box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
-    }
-}
 
-.bookmark-container {
-    position: absolute;
-    inset: .5rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-
-    .bookmark-header {
-        width: 100%;
-        height: 60px;
-        border-bottom: 1px solid #ddd;
     }
 
-    .bookmark-content {
-        flex: 1;
-        width: calc(100% - .5rem);
+    .bookmark-container {
+        position: absolute;
+        inset: .5rem;
         display: flex;
+        flex-direction: column;
         justify-content: flex-start;
         align-items: flex-start;
-        flex-wrap: wrap;
-        gap: .5rem;
-        position: relative;
-        padding: .5rem;
 
-        .bookmark-item {
-            cursor: pointer;
-            font-size: .75rem;
-            padding: .5rem;
-            position: relative;
-            z-index: 1;
-            border-radius: 5px;
-            transition: all 0.3s cubic-bezier(0.33, -0.16, 0.58, 1);
+        .bookmark-header {
+            width: 100%;
+            height: 60px;
+            border-bottom: 1px solid #ddd;
+        }
+
+        .bookmark-content {
+            flex: 1;
+            width: calc(100% - .5rem);
             display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-
-            &:hover {
-                box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
-                transform: scale(1.2);
-                background: rgba(255, 255, 255, 0.3);
-                font-weight: 600;
-                padding: .75rem;
-            }
-
-            .bookmark-icon {
-                width: 2.5rem;
-            }
-            .bookmark-title {
-                padding-top: .25rem;
-            }
+            justify-content: flex-start;
+            align-items: flex-start;
+            flex-wrap: wrap;
+            gap: .5rem;
+            position: relative;
+            padding: .5rem;
         }
     }
 }
