7c74209be7098654cfc70b6ce283a972a278e130
wzCoding <1429354968@qq.com>
2023-12-05 22:39:51 +0800

    submit the code

--- bookmark/src/main.js
+++ bookmark/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import '../public/styles/index.scss'
+
 const app = createApp(App)
-app.use(router).mount('#app')
+const pinia = createPinia()
+app.use(router).use(pinia).mount('#app')
