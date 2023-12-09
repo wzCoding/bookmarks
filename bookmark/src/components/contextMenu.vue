3759e8208ca5540020cb10a5938a6889722d0e23
wzCoding <1429354968@qq.com>
2023-12-09 23:23:37 +0800

    submit the code

--- bookmark/src/components/contextMenu.vue
+++ bookmark/src/components/contextMenu.vue
@@ -1,17 +1,39 @@
 <template>
-    <div class="context-menu">
+    <div class="context-menu" :style="styleObject">
         contextmenu
     </div>
 </template>
 <script setup>
-import { computed } from 'vue';
-import { ElConfigProvider, ElPagination } from 'element-plus';
-
+import { computed, ref } from 'vue';
 const props = defineProps({
     xAxis: { type: Number, default: 0 },
     yAxis: { type: Number, default: 0 }
 });
-
 const emits = defineEmits(['openContextMenu']);
+const left = ref(null);
+const top = ref(null);
+const styleObject = computed(() => {
+    return {
+        left: left.value ? `${left.value}px` : `${props.xAxis}px`,
+        top: top.value ? `${top.value}px` : `${props.yAxis}px`
+    }
+});
+
+defineExpose({ left, top })
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.context-menu {
+    position: absolute;
+    z-index: 1000;
+    background-color: #fff;
+    border: 1px solid #dcdfe6;
+    border-radius: 4px;
+    padding: 5px 0;
+    box-shadow: 0 2px 12px 0 rgb(0 0 0 / 10%);
+    /* 防止点击菜单后页面消失 */
+    pointer-events: none;
+    width: 200px;
+    height: 300px;
+    border-radius: 5px;
+}
+</style>
--- bookmark/src/views/HomeView.vue
+++ bookmark/src/views/HomeView.vue
@@ -3,7 +3,8 @@
         <VueDraggable ref="drag" v-model="currentMarks" @start="onDragStart" @end="onDragEnd" target=".book-transition"
             class="book-content">
             <TransitionGroup tag="div" name="fade" class="book-transition">
-                <BookMark v-for="bookmark in pageMarks" :key="bookmark.id" :bookmark="bookmark" @openUrl="openBookMark" @openContextMenu="openMenu">
+                <BookMark v-for="bookmark in pageMarks" :key="bookmark.id" :bookmark="bookmark" @openUrl="openBookMark"
+                    @openContextMenu="openMenu">
                 </BookMark>
             </TransitionGroup>
         </VueDraggable>
@@ -13,7 +14,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { createVNode, ref, render } from 'vue';
 import { usebookStore } from '@/store/usebookStore';
 import { usei18nStore } from '@/store/usei18nStore';
 import { VueDraggable } from 'vue-draggable-plus';
@@ -34,13 +35,13 @@ const sizeChange = (size) => {
 }
 const openBookMark = (param) => {
     if (param.url) {
-        if(param.openType !== "_newwindow"){
+        if (param.openType !== "_newwindow") {
             window.open(param.url, param.openType, "noopener,noreferrer");
-        }else{
+        } else {
             chrome.windows.create({
-                focused:true,
-                url:param.url,
-                state:"maximized",
+                focused: true,
+                url: param.url,
+                state: "maximized",
             })
         }
     } else {
@@ -48,9 +49,29 @@ const openBookMark = (param) => {
         bookStore.getCurrentMarks(param.id);
     }
 }
-const openMenu = (e)=>{
+let menuInstance = null;
+const createMenu = (props) => {
+    const container = document.createElement("div");
+    const vnode = createVNode(contextMenu, props);
+    render(vnode, container);
+    document.body.appendChild(container.firstElementChild);
+
+    const component = vnode.component;
+    return component.exposed
+}
+const openMenu = (e) => {
+    const props = {
+        xAxis: e.clientX,
+        yAxis: e.clientY,
+    }
     console.log(e)
-    console.log(contextMenu)
+    console.log(props)
+    if(menuInstance){
+        menuInstance.top.value = e.clientX
+        menuInstance.left.value = e.clientY
+    }else{
+        menuInstance = createMenu(props);
+    } 
 }
 const onDragStart = (e) => {
 
