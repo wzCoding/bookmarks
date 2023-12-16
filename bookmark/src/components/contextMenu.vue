568b62db5ae3f5779198a2a7cd2edb9e36867627
wzCoding <1429354968@qq.com>
2023-12-16 23:32:44 +0800

    基本展示功能完成

--- bookmark/src/components/contextMenu.vue
+++ bookmark/src/components/contextMenu.vue
@@ -14,8 +14,8 @@
 <script setup>
 import { computed, ref } from 'vue';
 import { ElIcon, ClickOutside as vClickOutside } from 'element-plus';
-import { Warning, Edit, Delete, Plus } from '@element-plus/icons-vue';
-const menuWidth = "100px"
+import { InfoFilled, Management, DeleteFilled, CirclePlusFilled } from '@element-plus/icons-vue';
+const menuWidth = "112px"
 const menuHeight = "144px"
 const props = defineProps({
     xAxis: { type: Number, default: 0 },
@@ -28,16 +28,16 @@ const timer = ref(null);
 const width = Number(menuWidth.replace("px", ""));
 const height = Number(menuHeight.replace("px", ""));
 const menuList = [
-    { label: "详细信息", icon: Warning, type: "info" },
-    { label: "编辑书签", icon: Edit, type: "edit" },
+    { label: "详细信息", icon: InfoFilled, type: "info" },
+    { label: "编辑书签", icon: Management, type: "edit" },
 ];
 if (props.target.children) {
-    menuList.push({ label: "添加书签", icon: Plus, type: "create" })
-    if (props.target.children.length !== 0) {
-        menuList.push({ label: "删除书签", icon: Delete, type: "delete" })
+    menuList.push({ label: "添加书签", icon: CirclePlusFilled, type: "create" })
+    if (props.target.children.length === 0) {
+        menuList.push({ label: "删除书签", icon: DeleteFilled, type: "delete" })
     }
 } else {
-    menuList.push({ label: "删除书签", icon: Delete, type: "delete" })
+    menuList.push({ label: "删除书签", icon: DeleteFilled, type: "delete" })
 }
 const styles = computed(() => {
     return {
@@ -60,7 +60,7 @@ const onClickOutside = () => {
 }
 const startTimer = () => {
     clearTimer();
-    //timer.value = setTimeout(closeMenu, 2000)
+    timer.value = setTimeout(closeMenu, 2000)
 }
 const clearTimer = () => {
     clearTimeout(timer.value)
@@ -88,38 +88,29 @@ defineExpose({ showMenu, closeMenu });
     z-index: 1000;
     background-color: #fff;
     box-shadow: 0 2px 12px 0 rgb(0 0 0 / 10%);
-    border-radius: 5px;
+    border-radius:1rem;
     display: flex;
-    justify-content: flex-start;
-    align-items: flex-start;
+    justify-content: space-around;
+    align-items: center;
     flex-direction: column;
     width: v-bind(menuWidth);
     height: v-bind(menuHeight);
-
+    box-sizing: border-box;
+    padding: 0.5rem;
     .menu-item {
-        width: 100%;
-        font-size: 0.75rem;
+        font-size: 0.85rem;
         color: #999;
         cursor: pointer;
-        padding: .5rem 0;
         display: flex;
         justify-content: center;
         align-items: center;
-
+        transition: all 0.2s;
         .menu-label {
-            padding-left: .25rem;
+            padding-left: 0.5rem;
         }
-
         &:hover {
             color: #409eff;
-        }
-
-        &:first-child {
-            padding-top: 1rem;
-        }
-
-        &:last-child {
-            padding-bottom: 1rem;
+            font-weight: 600;
         }
     }
 }
